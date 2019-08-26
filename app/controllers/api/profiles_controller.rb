1ff86fc77ce6d1a527ce69ade29a55cd217a2481
w-zengtao <so.zengtao@gmail.com>
2019-08-26 21:26:14 +0800

    [ADD] Update User Profile APi

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -5,18 +5,20 @@ module Api
 
     def show
       render json:
-        @current_user, serializer: ProfilesIndexSerializer
+        @current_user, serializer: ProfilesIndexSerializer, root: 'data', status: 200
     end
 
     # 商家更新自己的收款二维码
     def update
-
+      current_user.profile.update(profile_params)
+      render json:
+        @current_user.reload, serializer: ProfilesIndexSerializer, root: 'data', status: 201
     end
 
     private
 
-    def load_profile
-
+    def profile_params
+      params.require(:profile).permit(:wx, :zfb, :qq, :ysf, :jd)
     end
   end
 end
