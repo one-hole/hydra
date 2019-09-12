233ade9de8932a9e1bf06c4ca3b505daf2361402
w-zengtao <so.zengtao@gmail.com>
2019-09-12 17:12:45 +0800

    [FIX] 修复前端跨域的问题

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,13 @@ module Hydra
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
     ActiveModelSerializers.config.adapter = :json
   end
 end
